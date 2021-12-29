559b2c27632da0efda822285e92f29dfcc7bb702
smajumdar927 <58828730+smajumdar927@users.noreply.github.com>
2021-12-28 21:15:53 -0500

    ts

---
+++ cypress/integration/ts01.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress"/>
+
+describe("Test suit 01", () => {
+    before(() => {});
+  
+    beforeEach(() => {});
+    afterEach(() => {});
+  
+    after(() => {});
+  
+    describe("Test sub-suit 01", () => {
+      before(() => {});
+  
+      beforeEach(() => {});
+      afterEach(() => {});
+  
+      after(() => {});
+      it("Test 01", () => {});
+      it("Test 02", () => {});
+      it("Test 03", () => {});
+    });
+  
+    it("Test 01", () => {});
+    it("Test 02", () => {});
+    it("Test 03", () => {});
+  });
+  
+  describe("Test suit 02", () => {
+    it("Test 01", () => {});
+    it("Test 02", () => {});
+    it("Test 03", () => {});
+  });
+  
